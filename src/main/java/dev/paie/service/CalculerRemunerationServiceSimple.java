df91e7ce5af1fefe8f23be3d7a132662ef876411
AlexGeb <alex.behaghel@gmail.com>
2017-12-18 16:49:06 +0100

    petite modification de l'algo, avec un peu moins d'objets créés

--- src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
+++ src/main/java/dev/paie/service/CalculerRemunerationServiceSimple.java
@@ -2,7 +2,6 @@ package dev.paie.service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,44 +25,41 @@ public class CalculerRemunerationServiceSimple implements CalculerRemunerationSe
 
 		// SALAIRE_BASE
 		BigDecimal salaireDeBase = grade.getNbHeuresBase().multiply(grade.getTauxBase());
-		String salaireDeBaseStr = paieUtils.formaterBigDecimal(salaireDeBase);
-		BigDecimal salaireDeBaseRedefini = new BigDecimal(salaireDeBaseStr);
-		resultat.setSalaireDeBase(salaireDeBaseStr);
+		resultat.setSalaireDeBase(paieUtils.formaterBigDecimal(salaireDeBase));
+		BigDecimal salaireDeBaseArrondi = new BigDecimal(resultat.getSalaireDeBase());
 
 		// SALAIRE_BRUT
-		BigDecimal salaireBrut = salaireDeBaseRedefini.add(bulletin.getPrimeExceptionnelle());
-		String salaireBrutStr = paieUtils.formaterBigDecimal(salaireBrut);
-		BigDecimal salaireBrutRedefini = new BigDecimal(salaireBrutStr);
-		resultat.setSalaireBrut(salaireBrutStr);
-
+		BigDecimal salaireBrut = salaireDeBaseArrondi.add(bulletin.getPrimeExceptionnelle());
+		resultat.setSalaireBrut(paieUtils.formaterBigDecimal(salaireBrut));
+		BigDecimal salaireBrutArrondi = new BigDecimal(resultat.getSalaireBrut());
+		
 		// TOTAL_RETENUE_SALARIALE
 		List<Cotisation> cotisationsNonImposables = bulletin.getRemunerationEmploye().getProfilRemuneration()
 				.getCotisationsNonImposables();
 		BigDecimal totalRetenueSalariale = cotisationsNonImposables.stream().filter(c -> c.getTauxSalarial() != null)
-				.map(c -> c.getTauxSalarial().multiply(salaireBrutRedefini)).reduce((c1, c2) -> {
+				.map(c -> c.getTauxSalarial().multiply(salaireBrutArrondi)).reduce((c1, c2) -> {
 					return c1.add(c2);
 				}).orElse(new BigDecimal("0.00"));
-		String totalRetenueSalarialeStr = paieUtils.formaterBigDecimal(totalRetenueSalariale);
-		BigDecimal totalRetenueSalarialeRedefini = new BigDecimal(totalRetenueSalarialeStr);
-		resultat.setTotalRetenueSalarial(totalRetenueSalarialeStr);
-
+		resultat.setTotalRetenueSalarial(paieUtils.formaterBigDecimal(totalRetenueSalariale));
+		BigDecimal totalRetenueSalarialeArrondi = new BigDecimal(resultat.getTotalRetenueSalarial());
+		
 		// TOTAL_COTISATIONS_PATRONALES
 		BigDecimal totalCotisationsPatronales = cotisationsNonImposables.stream()
-				.filter(c -> c.getTauxPatronal() != null).map(c -> c.getTauxPatronal().multiply(salaireBrutRedefini))
+				.filter(c -> c.getTauxPatronal() != null).map(c -> c.getTauxPatronal().multiply(salaireBrutArrondi))
 				.reduce((c1, c2) -> {
 					return c1.add(c2);
 				}).orElse(new BigDecimal("0.00"));
 		resultat.setTotalCotisationsPatronales(paieUtils.formaterBigDecimal(totalCotisationsPatronales));
 
 		// NET_IMPOSABLE
-		BigDecimal netImposable = salaireBrutRedefini.subtract(totalRetenueSalarialeRedefini);
+		BigDecimal netImposable = salaireBrutArrondi.subtract(totalRetenueSalarialeArrondi);
 		resultat.setNetImposable(paieUtils.formaterBigDecimal(netImposable));
 
 		// NET_A_PAYER
 		List<Cotisation> cotisationsImposables = bulletin.getRemunerationEmploye().getProfilRemuneration()
 				.getCotisationsImposables();
 		BigDecimal totalCotisationsImposables = cotisationsImposables.stream().filter(c -> c.getTauxSalarial() != null)
-				.map(c -> c.getTauxSalarial().multiply(salaireBrutRedefini)).reduce((c1, c2) -> {
+				.map(c -> c.getTauxSalarial().multiply(salaireBrutArrondi)).reduce((c1, c2) -> {
 					return c1.add(c2);
 				}).orElse(new BigDecimal("0.00"));
 		BigDecimal netAPayer = netImposable.subtract(totalCotisationsImposables);
